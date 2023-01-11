621ed25d9e2455d20c43c1a288303e0c493eb296
Starcea <stardev.uwu@gmail.com>
2023-01-11 15:00:57 +0900

    feat: tray icon

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
-import { app, BrowserWindow } from "electron"
+import { app, BrowserWindow, Menu, Tray } from 'electron'
+import path from 'path'
 
-app.on("ready", () => {
-    let window = new BrowserWindow({
+let window: BrowserWindow
+
+app.on('ready', () => {
+    window = new BrowserWindow({
         show: false
     })
     window.maximize()
-    window.loadURL("https://pikokr.dev/")
+    window.loadURL('https://pikokr.dev/')
         .catch((err) => {
             console.error(err)
         })
+})
+
+app.whenReady().then(() => {
+    let tray = new Tray(path.dirname(path.basename(__dirname)) + '/assets/icon.ico')
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Chat', type: 'normal', click: () => {
+                window.loadURL('https://pikokr.dev/my/messaging')
+                window.focus()
+            }
+        },
+        {
+            label: 'Notification', type: 'normal', click: () => {
+                window.loadURL('https://pikokr.dev/my/notifications')
+                window.focus()
+            }
+        }
+    ])
+    tray.setToolTip('Pikokr Misskey')
+    tray.setContextMenu(contextMenu)
 })
